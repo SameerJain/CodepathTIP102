fb35502fed162e3d397d441cd60871512d663a1f
Sameer Jain <sameerjain501@gmail.com>
2026-02-03 23:18:28 -0500

    basic code formatting

---
+++ unit_1/standard_1.py
@@ -0,0 +1,35 @@
+class problem_1:
+    def welcome(self):
+        print("Welcome to The Hundred Acre Wood!")
+
+    def test_1(self):
+        print("Expected: ")
+        self.welcome()
+
+class problem_2:
+    def greeting(self,name):
+        print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin")
+
+    def test_1(self):
+        self.greeting("Michael")
+    
+    def test_2(self):
+        self.greeting("Christopher Robin")
+
+class problem_3: 
+    def print_catchphrase(character):
+        phrase_list = {"Pooh":"Oh bother!",
+                    "Tigger":"TTFN: Ta-ta for now!",
+                    "Eeyore":"Thanks for noticing me.",
+                    "Christopher Robin":"Silly old bear."}
+
+# class problem_4:
+#     def (sum_hunny_jars):
+    
+#     def test_1:
+
+#     def test_2:
+        
+
+
+# class problem_5:
